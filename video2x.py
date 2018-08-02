4eec3e3b9f9c230aa346d77229c275f9a8ea94ca
K4YT3X <k4t@protonmail.com>
2018-08-01 21:40:48 -0400

    fixed json output deocde error (by @Mirrorman95)

--- video2x.py
+++ video2x.py
@@ -38,7 +38,7 @@ import shutil
 import subprocess
 import traceback
 
-VERSION = '2.0 beta'
+VERSION = '2.0.1'
 
 EXEC_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 FRAMES = '{}\\frames'.format(EXEC_PATH)  # Folder containing extracted frames
@@ -95,7 +95,7 @@ def get_vid_info():
     """
     json_str = subprocess.check_output(
         '{} -v quiet -print_format json -show_format -show_streams {}'.format('\"' + FFMPEG_PATH + 'ffprobe.exe\"', args.video))
-    return json.loads(json_str)
+    return json.loads(json_str.decode('utf-8'))
 
 
 def check_model_type(args):
