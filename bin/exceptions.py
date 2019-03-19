3fc719aee7263b16ad8e68dad07ae42bd40f32b3
k4yt3x <k4yt3x@k4yt3x.com>
2019-03-19 13:07:03 -0400

    removed invalid argument exception

--- bin/exceptions.py
+++ bin/exceptions.py
@@ -4,15 +4,10 @@
 Name: Video2X Exceptions
 Dev: K4YT3X
 Date Created: December 13, 2018
-Last Modified: December 19, 2018
+Last Modified: March 19, 2019
 """
 
 
 class ArgumentError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-
-
-class InvalidModelType(Exception):
     def __init__(self, message):
         super().__init__(message)
