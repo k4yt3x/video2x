4973ae5b8d665ed7a5240c1d7d881035538196c2
k4yt3x <k4yt3x@k4yt3x.com>
2019-03-31 02:17:10 -0400

    made video2x compatible with pyinstaller

--- bin/video2x.py
+++ bin/video2x.py
@@ -46,6 +46,7 @@ import json
 import os
 import psutil
 import shutil
+import sys
 import tempfile
 import time
 import traceback
@@ -78,7 +79,7 @@ def process_arguments():
     upscaler_options.add_argument('-d', '--driver', help='Waifu2x driver', action='store', default='waifu2x_caffe', choices=['waifu2x_caffe', 'waifu2x_converter'])
     upscaler_options.add_argument('-y', '--model_dir', help='Folder containing model JSON files', action='store')
     upscaler_options.add_argument('-t', '--threads', help='Number of threads to use for upscaling', action='store', type=int, default=5)
-    upscaler_options.add_argument('-c', '--config', help='Video2X config file location', action='store', default='{}\\video2x.json'.format(os.path.dirname(os.path.abspath(__file__))))
+    upscaler_options.add_argument('-c', '--config', help='Video2X config file location', action='store', default='{}\\video2x.json'.format(os.path.dirname(os.path.abspath(sys.argv[0]))))
     upscaler_options.add_argument('-b', '--batch', help='Enable batch mode (select all default values to questions)', action='store_true')
 
     # scaling options
