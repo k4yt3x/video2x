b6b1bf9f0e46fa1f8854aee81cc6a5e1a5862407
k4yt3x <i@k4yt3x.com>
2022-02-15 00:52:28 +0000

    added two container debugging scripts

---
+++ scripts/run-interactive-container.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# mount the current (video2x repo root) directory into a container
+# with drivers installed so the code can be debugged in the container
+# this one launches an interactive shell instead of Python
+
+set -euo pipefail
+
+sudo podman run -it --rm \
+    --gpus all -v /dev/dri:/dev/dri \
+    -v $PWD:/host \
+    -m 15g \
+    --cpus 0.9 \
+    -v $HOME/projects/media2x/video2x:/video2x \
+    -e PYTHONPATH="/video2x" \
+    --entrypoint=/bin/bash \
+    ghcr.io/k4yt3x/video2x:5.0.0-beta1-cuda
+
+# alias upscale='python3 -m video2x -i /host/input-large.mp4 -o /host/output-large.mp4 -p5 upscale -h 1440 -d waifu2x -n3'
---
+++ scripts/run-source-in-container.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# mount the current (video2x repo root) directory into a container
+# with drivers installed so the code can be debugged in the container
+
+set -euo pipefail
+
+sudo podman run -it --rm \
+    --gpus all -v /dev/dri:/dev/dri \
+    -v $PWD:/host \
+    -m 15g \
+    --cpus 0.9 \
+    -v $HOME/projects/media2x/video2x:/video2x \
+    -e PYTHONPATH="/video2x" \
+    ghcr.io/k4yt3x/video2x:5.0.0-beta2-cuda \
+    -i input-large.mp4 -o output-large.mp4 \
+    -p5 \
+    upscale \
+    -h 1440 -d waifu2x -n3
