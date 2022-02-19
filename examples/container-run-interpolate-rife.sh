2510c8fa28ce9fd86157adef9c13a17e92365a9c
k4yt3x <i@k4yt3x.com>
2022-02-19 03:21:47 +0000

    bumped script versions to beta4

--- examples/container-run-interpolate-rife.sh
+++ examples/container-run-interpolate-rife.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 sudo podman run \
     -it --rm --gpus all -v /dev/dri:/dev/dri \
     -v $PWD/data:/host \
-    ghcr.io/k4yt3x/video2x:5.0.0-beta3-cuda \
+    ghcr.io/k4yt3x/video2x:5.0.0-beta4-cuda \
     -i input.mp4 -o output.mp4 \
     interpolate
