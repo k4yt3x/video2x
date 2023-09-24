b4b7cadf0d51dd8bfdea719e0aa48ec04ff0f57c
k4yt3x <i@k4yt3x.com>
2023-09-24 07:33:07 +0000

    tests(*): updated test file paths and removed unused dependencies
    Signed-off-by: k4yt3x <i@k4yt3x.com>

--- tests/test_upscaler.py
+++ tests/test_upscaler.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import ctypes
-import multiprocessing
-import os
 from pathlib import Path
 
 import utils
@@ -14,9 +11,11 @@ from video2x import Upscaler, Video2X
 
 def test_upscaling():
     video2x = Video2X()
-    output_path = Path("data/test_video_output.mp4")
+
+    output_path = Path("tests/data/test_video_output.mp4")
+
     video2x.upscale(
-        Path("data/test_video.mp4"),
+        Path("tests/data/test_video.mp4"),
         output_path,
         None,
         720,
@@ -32,9 +31,9 @@ def test_upscale_image():
     # initialize upscaler instance
     upscaler = Upscaler()
 
-    image = Image.open("data/test_image.png")
+    image = Image.open("tests/data/test_image.png")
     upscaled_image = upscaler.upscale_image(image, 1680, 960, "waifu2x", 3)
-    reference_image = Image.open("data/test_image_ref.png")
+    reference_image = Image.open("tests/data/test_image_ref.png")
     assert utils.get_image_diff(upscaled_image, reference_image) < 0.5
 
 
